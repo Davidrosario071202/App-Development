245adde116d0c751ce01a0e4b1966feb28ac2b03
Davidrosario071202 <121172372+Davidrosario071202@users.noreply.github.com>
2023-10-05 12:26:44 +0530

    Add files via upload

---
+++ backend/day1 backend/Book-Store/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,45 @@
+package com.example.demo.service;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepo;
+
+@Service
+public class UserService {
+    private final UserRepo userRepository;
+
+    @Autowired
+    public UserService(UserRepo userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User userEntity) {
+        return userRepository.save(userEntity);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User updateUser(User userEntity) {
+        return userRepository.save(userEntity);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    // Other user-related methods if needed
+}
